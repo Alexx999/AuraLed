4d6e4516d218ba641884458c600dabd43950d970
Alexander Vostres <alex.vostres@gmail.com>
2016-10-30 03:40:36 +0200

    Initial commit

---
+++ AuraLedHelper/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace AsusLedHelper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private FieldInfo _fieldInfo;
+
+        public App()
+        {
+            var actualValue = DesignerProperties.IsInDesignModeProperty.DefaultMetadata.DefaultValue;
+            UpdateDesignerMode(true);
+            CheckLicense(Xceed.Wpf.Toolkit.Licenser.LicenseKey);
+            UpdateDesignerMode((bool)actualValue);
+
+            TextOptions.TextFormattingModeProperty.OverrideMetadata(typeof(DependencyObject),
+                new FrameworkPropertyMetadata(TextFormattingMode.Display, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.Inherits));
+            InitializeComponent();
+
+
+            var service = FirstFloor.XamlSpy.Services.XamlSpyService.Current;
+            service.Connect("127.0.0.1", 4530, "01311");
+        }
+
+        private void CheckLicense(string licenseKey)
+        {
+            //Sorry - I don't think that using XAML inspectors requires "Plus" but due to bug in licensing it won't work without this hack
+        }
+
+        private void UpdateDesignerMode(bool value)
+        {
+            var metadata = DesignerProperties.IsInDesignModeProperty.DefaultMetadata;
+            var field = _fieldInfo ?? (_fieldInfo = typeof(PropertyMetadata).GetField("_defaultValue", BindingFlags.Instance | BindingFlags.NonPublic));
+            field?.SetValue(metadata, value);
+        }
+    }
+}
---
+++ AuraLedHelperService/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsusLedHelperService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new AuraLedHelperService()
+            };
+            ServiceBase.Run(ServicesToRun);
+
+            /*
+            axdata axdata = new axdataClass();
+            axdata.iAcpiSetItem(0x13060041, 0, 1);
+            axdata.iAcpiSetItem(0x13060042, 4, 1);*/
+        }
+    }
+}
