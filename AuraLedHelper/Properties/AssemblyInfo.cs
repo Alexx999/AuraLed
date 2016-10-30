bf349024df26b0deaf976fafe90d4eb2fe7a07a5
Alexander Vostres <alex.vostres@gmail.com>
2016-10-30 15:02:57 +0200

    remove xceed config

--- AuraLedHelper/Properties/AssemblyInfo.cs
+++ AuraLedHelper/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyConfiguration("XCEED-SAMPLE-APPLICATION")]
+[assembly: AssemblyConfiguration("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
