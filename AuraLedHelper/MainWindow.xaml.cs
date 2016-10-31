3e0296134b548da22fa8fee06196573e1a9b67da
Alexander Vostres <alex.vostres@gmail.com>
2016-10-31 12:40:08 +0200

    Add about dialog

--- AuraLedHelper/MainWindow.xaml.cs
+++ AuraLedHelper/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace AuraLedHelper
+﻿using System.Windows;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace AuraLedHelper
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +11,11 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void AboutClick(object sender, RoutedEventArgs e)
+        {
+            await this.ShowMessageAsync("About", "Created by Alexander Vostres\nalex.vostres@gmail.com\ngithub.com/Alexx999/AuraLed");
         }
     }
 }
